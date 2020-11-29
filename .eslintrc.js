1b22e49f8dfe66527fae1c65abd1e0af04135100
kimura keisuke <mail@kkeisuke.com>
2020-11-29 16:03:41 +0900

    lint 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -37,13 +31,10 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
     }
   ]
-};
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,8 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  // https://github.com/vuejs/vue-cli/releases/tag/v4.5.8
+  // lint が通らないので保留
+  // const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent
   export default component
 }
