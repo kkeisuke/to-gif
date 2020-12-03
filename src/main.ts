489184dc2b9af94afdb7cff9119429992479b71d
kimura keisuke <mail@kkeisuke.com>
2020-12-03 21:01:50 +0900

    primevue 関連追加

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
 createApp(App).mount('#app')
