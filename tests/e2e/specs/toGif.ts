5b88e4feb6979121c901659db9262e9f51f558db
kimura keisuke <mail@kkeisuke.com>
2020-12-11 17:28:06 +0900

    lint 修正

--- tests/e2e/specs/toGif.ts
+++ tests/e2e/specs/toGif.ts
@@ -6,7 +6,7 @@ describe('toGif', () => {
 
   it('video convert', () => {
     cy.get('#description').should('have.css', 'display', 'block')
-    cy.get('#FileUpLoad').attachFile({filePath: 'mock.mp4', encoding: 'binary', mimeType: 'application/octet-stream' }, { subjectType: 'drag-n-drop' })
+    cy.get('#FileUpLoad').attachFile({ filePath: 'mock.mp4', encoding: 'binary', mimeType: 'application/octet-stream' }, { subjectType: 'drag-n-drop' })
     cy.get('#imageName').contains('mock.mp4.gif')
     cy.get('#imageSize').contains('78 KB')
     cy.get('#preview').should('have.css', 'display', 'block')
