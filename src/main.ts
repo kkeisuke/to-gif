4a34b9ad1f07fc4e3e04a496bdc5a25b3b60b380
kimura keisuke <mail@kkeisuke.com>
2021-05-03 11:37:50 +0900

    依存関係更新

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/saga-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
 import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
 
 import '@/styles/layout.scss'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
