94958f2d170f756aea83c59874a9e857e6bb233c
kimura keisuke <mail@kkeisuke.com>
2021-08-14 23:01:46 +0900

    SharedArrayBuffer の cross-origin に関する修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   productionSourceMap: false,
-
+  devServer: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
   pwa: {
     name: 'ToGIF',
     themeColor: '#336699'
